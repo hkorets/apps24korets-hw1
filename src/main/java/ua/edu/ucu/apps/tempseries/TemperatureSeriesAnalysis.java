9ec0f50dfd9e8c37f452bfe6af9e556a89ec8f15
Khrystya Korets <hkorets0411@gmail.com>
2024-10-06 16:59:32 +0300

    Update TemperatureSeriesAnalysis.java

--- src/main/java/ua/edu/ucu/apps/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/apps/tempseries/TemperatureSeriesAnalysis.java
@@ -153,10 +153,22 @@ public class TemperatureSeriesAnalysis {
     }
 
     public double[] sortTemps() {
-        double[] sorted = temperatureSeries.clone();
-        Arrays.sort(sorted);
-        return sorted;
+    double[] sorted = temperatureSeries.clone();
+    int n = sorted.length;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (sorted[j] > sorted[j + 1]) {
+                double temp = sorted[j];
+                sorted[j] = sorted[j + 1];
+                sorted[j + 1] = temp;
+            }
+        }
     }
+
+    return sorted;
+}
+
 
     public TempSummaryStatistics summaryStatistics() {
         if (size == 0) {
